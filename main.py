7f1bd05c19cb0065dd7131e338a3f0e22aaaef88
Kyle <notxap55@yahoo.com>
2024-07-17 16:25:32 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import colorgram
+
+# rgb_colors = []
+# colors = colorgram.extract('image.jpg', 10)
+# for color in colors:
+#     color_tuple = (color.rgb.r, color.rgb.g, color.rgb.b)
+#     rgb_colors.append(color_tuple)
+
+color_list = [(222, 163, 66), (19, 45, 87), (136, 61, 84), (177, 60, 44), (239, 230, 223), (126, 40, 61), (21, 86, 61), (59, 48, 37)]
+
