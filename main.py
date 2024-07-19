747af541a8681e126093817a607919678b80cbae
Kyle <notxap55@yahoo.com>
2024-07-19 14:56:45 -0700

    completed

--- main.py
+++ main.py
@@ -1,10 +1,41 @@
-import colorgram
+from turtle import *
+import turtle
+import random
 
-# rgb_colors = []
-# colors = colorgram.extract('image.jpg', 10)
-# for color in colors:
-#     color_tuple = (color.rgb.r, color.rgb.g, color.rgb.b)
-#     rgb_colors.append(color_tuple)
 
-color_list = [(222, 163, 66), (19, 45, 87), (136, 61, 84), (177, 60, 44), (239, 230, 223), (126, 40, 61), (21, 86, 61), (59, 48, 37)]
+def place_dot():
+    tim.pencolor(random.choice(color_list))
+    tim.hideturtle()
+    tim.pendown()
+    tim.dot(dot_size)
+    tim.penup()
+    tim.forward(dot_space)
 
+
+def new_line():
+    x_cor = tim.xcor()
+    y_cor = tim.ycor()
+    tim.teleport(x_cor - 500, y_cor + 50)
+
+
+color_list = [(222, 163, 66), (19, 45, 87), (136, 61, 84), (177, 60, 44), (239, 230, 223), (126, 40, 61), (21, 86, 61),
+              (59, 48, 37)]
+x_pos = -250
+y_pos = -250
+dot_size = 20
+dot_space = 50
+
+
+turtle.colormode(255)
+tim = Turtle()
+tim.penup()
+tim.setx(x_pos)
+tim.sety(y_pos)
+
+for _ in range(10):
+    for _ in range(10):
+        place_dot()
+    new_line()
+
+screen = Screen()
+screen.exitonclick()
